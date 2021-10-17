9ebcdf25fdf581d18b9c76ead2159db64a16b00d
Sally Lait <sally@recordssoundthesame.com>
2021-10-17 10:22:55 +0100

    Move footer markup and tweak styles

--- src/App.js
+++ src/App.js
@@ -6,23 +6,20 @@ import { Reviews } from "./Reviews";
 function App() {
   return (
     <div className="App">
-
       <div className="container">
 
-      <header>
-          <h1 className="logo">Small reviews</h1>
-      </header>
+        <header>
+            <h1 className="logo">Small reviews</h1>
+        </header>
 
         <div className="container-inner">
-
-        <Reviews />
+          <Reviews />
+        </div>
 
         <footer>
           <p>Site by <a href="https://sally.dev">Sally</a> for herself</p>
         </footer>
-
-        </div>
-        </div>
+      </div>
 
     </div>
   );
