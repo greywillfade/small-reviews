c6cf467aa78cd157b3bd98f035a8606d363d78d4
Sally Lait <2871272+greywillfade@users.noreply.github.com>
2022-10-09 20:54:14 +0100

    Add The best, most awful job

--- src/data.js
+++ src/data.js
@@ -1,5 +1,12 @@
 export const reviewData = [
     {
+        title: "The best, most awful job",
+        author: "Katherine May",
+        category: "Book",
+        dateFinished: "09-Oct-2022",
+        rating: "4",
+        thoughts: "I was given this by a very kind friend on returning home from hospital with my kid. And to be honest, since then I've been a bit scared to start it. When I looked at the title, the 'most awful' part always jumped out, and I was afraid that it'd affirm fears and limitations and upset. But it was in fact quite the opposite. In this collection of essays around motherhood (in all kinds of forms) I unexpectedly found so much resonated, but it was instead reaffirming and gave voice to many things I'd experienced and felt but maybe hadn't had the words for. I wanted to underline things to come back to, but there'd have been so much. It's a book that I'd give someone else, in the hope that they too may take so much from it."
+    },{
         title: "Project Hail Mary",
         author: "Andy Weir",
         category: "Book",
