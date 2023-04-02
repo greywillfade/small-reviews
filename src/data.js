147fb34d44eab274278c86e4e1a567655b9515ff
Sally Lait <2871272+greywillfade@users.noreply.github.com>
2023-04-02 20:18:45 +0100

    Add Expanse 5

--- src/data.js
+++ src/data.js
@@ -1,5 +1,13 @@
 export const reviewData = [
     {
+        title: "Nemesis Games (The Expanse book 5)",
+        author: "James S. A. Corey",
+        category: "Book",
+        dateFinished: "28-Mar-2023",
+        rating: "4",
+        thoughts: "After the previous one, I flicked through this before I started and was excited to see all the chapters were from the perspective of the core group... hooray! It delivered too, I actually felt like this was probably more effective than the TV show in describing how everyone split up and went on their own journeys. Naomi's journey in particular was really great to revisit in written form."
+    },
+    {
         title: "Cibola Burn (The Expanse book 4)",
         author: "James S. A. Corey",
         category: "Book",
