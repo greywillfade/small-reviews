063fcdbe634acdc158ad6c5708b51bdfa25faeca
Sally Lait <2871272+greywillfade@users.noreply.github.com>
2023-07-14 21:03:59 +0100

    Add the three body problem

--- src/data.js
+++ src/data.js
@@ -1,5 +1,12 @@
 export const reviewData = [
     {
+        title: "The Three-body Problem",
+        author: "Liu Cixin",
+        category: "Book",
+        dateFinished: "14-Jul-2023",
+        rating: "3.5",
+        thoughts: "Another book my husband never finished and I eventually nicked. He'd bought three in the series and there was enough in this to move on to them at some point, but it wasn't a classic for me. It felt a bit clunky to read at points, in the way that translated things often do, but I appreciated the translator's postscript saying that 'the reader sees a glimpse of another culture's pattern of thinking, hears an echo of another language's rhythms and cadences, and feels a tremor of another people's gestures and movements'. The strange in-game scenes reminded me of something else but I can't remember what, and I enjoyed flicking between that and the real world with everything explained by the end, but I did find some of the science a bit hard to wrap my tired brain around at points. Still, some interesting concepts and I'll read on."
+    },{
         title: "Hi-fi Rush",
         category: "Game",
         dateFinished: "13-Jul-2023",
